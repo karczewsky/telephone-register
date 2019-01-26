21d5a2f410e158dab54aae40078bab32fe0a71a2
wolpear <wolpear@users.noreply.github.com>
2019-01-26 13:57:42 +0100

    :boom: add event class

---
+++ src/main/java/application/Event.java
@@ -0,0 +1,48 @@
+package application;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Event {
+    @Id
+    private long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "fk_start")
+    private Phone from_phone;
+
+    @ManyToOne()
+    @JoinColumn(name = "fk_to")
+    private Phone to_phone;
+
+    private Date start;
+    private Date finish;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        ANSWERED,
+        UNANSWERED;
+
+        Status readFromUser() {
+            boolean err = false;
+            while (true) {
+                if (err)
+                    System.out.println("Nieznana opcja podana.");
+                System.out.println("Czy polaczenie odebrane[tak/nie]:");
+
+                String line = Main.userInput.nextLine().toLowerCase().trim();
+
+                if (line.equals("tak") || line.equals("t"))
+                    return ANSWERED;
+
+                if (line.equals("nie") || line.equals("n"))
+                    return UNANSWERED;
+
+                err = true;
+            }
+        }
+    }
+}
